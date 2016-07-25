dd960e8074e8f95e6eaab109704a9c4e9eea72a6
Lucas Ferreira <lucasfercunha@gmail.com>
2016-07-25 11:46:57 -0300

    Laravel clean installation

---
+++ config/modules.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Modular Configuration
+    |--------------------------------------------------------------------------
+    |
+    | This option controls where the application modules will be locates. The
+    | `available` array lists all modules that should be loaded when the
+    | service provider boots up. The key from the enabled array determines the
+    | priority loading the modules.
+    |
+    */
+    'namespace' => 'App\\Modules',
+    'path' => app_path('Modules'),
+
+    'available' => [
+        10 => 'backend',
+    ],
+
+];
